c4c7c805467c8fdac8c65ccd6ce6e5d354ccce14
Milan Sony <milansonyofficial@gmail.com>
2026-03-13 14:09:20 +0530

    feat: express body parser added

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import express from "express";
-
 import { baseRoute } from "./routes/index.js";
 
 // Creates an express app
 const app = express();
 
-// Base URL
-app.use("/", baseRoute)
+// Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen((process.env.PORT || 5000), () => {
-    console.log(`\n🚀 Server listening on port: ${process.env.PORT || 5000}`);
+// Base route
+app.use("/", baseRoute);
+
+const PORT = Number(process.env.PORT || 5000);
+app.listen(PORT, () => {
+    console.log(`\n🚀 Server listening on port: ${PORT}`);
 });

d7b7608d11f216e8c371dfd2b5712bd801c10a8c
Milan Sony <milansonyofficial@gmail.com>
2026-03-13 17:48:40 +0530

    fix: removed dotenv configs

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 import express from "express";
-import dotenv from "@dotenvx/dotenvx";
 
 import { baseRoute } from "./routes/index.js";
 import { connectDB } from "./configs/mongoDBConfig.js";
-
-if (process.env.NODE_ENV !== "production") {
-    dotenv.config();  // Load local .env secrets
-}
 
 // Creates an express app
 const app = express();

86d2b1def711b1418b8165d20f65779c779d137b
Milan Sony <milansonyofficial@gmail.com>
2026-03-20 15:15:04 +0530

    feat: crud task

--- routes/api/task.js
+++ routes/api/task.js
@@ -1,11 +1,19 @@
 import express from "express";
-import { createTasks, getTasks } from "../../controllers/taskControllers.js";
+import { createTasks, deleteTasks, getTasks, updateTasks } from "../../controllers/taskControllers.js";
 import { verifyToken } from "../../middleware/verifyToken.js";
 
 const taskRoutes = express.Router();
 
 // Create tasks
 taskRoutes.post("/setTasks", verifyToken, createTasks);
+
+// Get all tasks
 taskRoutes.get("/getTasks", verifyToken, getTasks);
+
+// Update the tasks
+taskRoutes.put("/updateTasks", verifyToken, updateTasks);
+
+// Delete the tasks
+taskRoutes.delete("/deleteTasks/:id", verifyToken, deleteTasks);
 
 export default taskRoutes;
